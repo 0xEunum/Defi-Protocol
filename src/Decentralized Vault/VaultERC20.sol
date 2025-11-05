bb61dbd25ab5433a53dd26eb1a9c1838ad83e973
0xEunum <0xEunum@gmail.com>
2025-11-05 22:21:26 +0530

    feat: add mint and burn functionality

--- src/Decentralized Vault/VaultERC20.sol
+++ src/Decentralized Vault/VaultERC20.sol
@@ -5,4 +5,12 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VaultERC20 is ERC20 {
     constructor() ERC20("VaultERC20", "VT") {}
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) external {
+        _burn(_account, _amount);
+    }
 }
