7d36604f86f34b08a40b56b038740a3f5cd2d75e
0xEunum <0xEunum@gmail.com>
2025-11-07 23:54:07 +0530

    feat: add local deployment

--- script/Decentralized Vault/DeployVault.s.sol
+++ script/Decentralized Vault/DeployVault.s.sol
@@ -8,8 +8,19 @@ import {VaultERC20} from "src/Decentralized Vault/VaultERC20.sol";
 contract DeployVault is Script {
     Vault vault;
     VaultERC20 token;
+    uint256 constant ANVIL_CHAIN_ID = 31337;
 
     function run() external returns (Vault, VaultERC20) {
+        if (block.chainid == ANVIL_CHAIN_ID) {
+            token = new VaultERC20();
+
+            uint256 apr = 5e16;
+            uint256 rps = apr / 31536000;
+
+            vault = new Vault(address(token), rps);
+            return (vault, token);
+        }
+
         vm.startBroadcast();
 
         token = new VaultERC20();
